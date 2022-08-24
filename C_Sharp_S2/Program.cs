ed28140e1f586e5131d9fa1cd824d69b5f468b8f
Someone <someone@example.com>
2022-08-24 19:42:04 +0300

    S2 HW 13 done

--- C_Sharp_S2/Program.cs
+++ C_Sharp_S2/Program.cs
@@ -19,6 +19,35 @@ int result = findSecNum(num);
 Console.Write($"Second number is {result} of {num}");
 */
 
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+/*
+void showThirdNum(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("No third digit");
+    }
+    else
+    {
+    while(num > 999)
+    {
+       num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine($"Third number is {num}");
+    }
+}
+
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+showThirdNum(num);
+*/
 
 
 
