3e35a8ce2d9dba5895f45947e05f39bcee22ad9e
Someone <someone@example.com>
2022-08-24 19:40:58 +0300

    S2 HW 10 done

--- C_Sharp_S2/Program.cs
+++ C_Sharp_S2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+/*
+int findSecNum(int num)
+{
+    int secondThird = num % 100;
+    int second = secondThird / 10;
+    return second;
+}
+
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = findSecNum(num);
+Console.Write($"Second number is {result} of {num}");
+*/
+
+
+
+
+
