585f0e97508c243d5b69eaa76e2ae568257de48a
Someone <someone@example.com>
2022-09-24 20:53:07 +0300

    S6 hw

---
+++ C_Sharp_S6/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*
+int PositiveNums(int size)
+{
+    int count = 0;
+    int[] arr = new int[size];
+    if (size < 1)
+    {
+        Console.WriteLine("Введите мин значение 1");
+    }
+    for(int i = 0; i < arr.Length; i++)
+    {
+
+        Console.Write($"Введите {i + 1}е число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    for(int i = 0; i < arr.Length; i++)
+    { 
+        if(arr[i] > 0 ) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Количество чисел больше 0: {PositiveNums(size)}");
+*/
+
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями  y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
